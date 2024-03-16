71058ff7a1da88ef6a08cfa0bd355306df9f8b15
Claudionor Junior da Rosa Nilson <juniordarosa64@gmail.com>
2024-03-16 15:42:11 -0300

    Translate for english

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const key = '7c9ec2819947ae54ec74d93cd68fb94d'
 const page = document.querySelector('.page')
 const div = document.createElement('div')
 
-document.querySelector('.ajuste input').addEventListener('keypress', function(event){
+document.querySelector('.searchCity input').addEventListener('keypress', function(event){
   if(event.key === 'Enter') {
     valueInput()
   }
@@ -11,13 +11,13 @@ document.querySelector('.ajuste input').addEventListener('keypress', function(ev
 function valueInput() {
   div.innerHTML = ''
   
-  var input = document.querySelector('div input').value
+  var input = document.querySelector('.searchCity input').value
   if(input.length == 0){
     alert('Digite o nome de alguma cidade')
     return
   }
   
-  document.querySelector('div input').value = ''
+  document.querySelector('.searchCity input').value = ''
   dados(input)
 }
 
@@ -36,38 +36,38 @@ function createRow(dados) {
   div.innerHTML = `
   <p class="nameCity">${dados.name}</p> 
   <section>
-    <div class="temperatura">
+    <div class="temperature">
       <img class="imageTemp" src="https://openweathermap.org/img/wn/${dados.weather[0].icon}.png" alt="">
       <div class="tempName">
         <p class="temp">${dados.main.temp.toFixed(1)}°C</p>
-        <p class="estado">${dados.weather[0].description}</p> 
+        <p class="state">${dados.weather[0].description}</p> 
       </div>
     </div> 
-    <div class="quadrados" id="quadrado0">
-      <div class="quadrado">
+    <div class="boxes" id="box1">
+      <div class="box">
         <img src="img/Humidity.png" alt="">
-        <div class="ajusteQuadrado"> 
+        <div class="divBox"> 
           <P>Umidade</P>
-          <span class="umidade">${dados.main.humidity}%</span> 
+          <span class="">${dados.main.humidity}%</span> 
         </div>    
       </div>
-      <div class="quadrado" id="quadrado1">
-        <img src="img/vento.png" alt="">
-        <div class="ajusteQuadrado"> 
+      <div class="box" id="box2">
+        <img src="img/wind.png" alt="">
+        <div class="divBox"> 
           <P>Vento</P>
           <span class="">${dados.wind.speed.toFixed(1)}Km</span> 
         </div>    
       </div>  
-      <div class="quadrado" id="quadrado2">
-        <img src="img/quente.png" alt="">
-        <div class="ajusteQuadrado"> 
+      <div class="box" id="box3">
+        <img src="img/tempHot.png" alt="">
+        <div class="divBox"> 
           <P>Temp Máx</P>
           <span class="tempMax">${dados.main.temp_max.toFixed(1)}°C</span> 
         </div>    
       </div>  
-      <div class="quadrado" id="quadrado3">
-        <img src="img/frio.png" alt="">
-        <div class="ajusteQuadrado"> 
+      <div class="box" id="box4">
+        <img src="img/tempCold.png" alt="">
+        <div class="divBox"> 
           <P>Temp Min</P>
           <span class="tempMin">${dados.main.temp_min.toFixed(1)}°C</span> 
         </div>    
